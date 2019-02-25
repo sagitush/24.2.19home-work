337926ce6a7e24c90c1bb158bcdcbd3275570abc
sagitush <44786027+sagitush@users.noreply.github.com>
2019-02-25 20:58:20 +0200

    Add files via upload

---
+++ 24.2.19 homework/24.2.19 homework/Car.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _24._2._19_homework
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public int Brand { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        private int codan;
+        protected int numberOfSeats;
+        
+
+        public Car()
+        {
+
+        }
+        public Car(string FileName)
+        {
+            Car car;
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car));
+            using (Stream file = new FileStream($@"c:\{FileName}\xmlfile.xml", FileMode.Open))
+            {
+                car = myxmlSerializer.Deserialize(file) as Car;
+            }
+            this.Model = car.Model;
+            this.Brand = car.Brand;
+            this.Year = car.Year;
+            this.Color = car.Color;
+        }
+
+        public Car(string model, int brand, int year, string color, int codan, int numberOfSeats)
+        {
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+            this.codan = codan;
+            this.numberOfSeats = numberOfSeats;
+        }
+        public int Codan
+        {
+            get
+            {
+                return this.codan;
+            }
+        }
+        public int NumberOfSeats
+        {
+            get
+            {
+                return this.numberOfSeats;
+            }
+        }
+        protected void ChangeNumberOfSeats(int n)
+        {
+            this.numberOfSeats = n;
+        }
+        public static void SerializeACar(string fileName,Car car)
+        {
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car));
+            using (Stream file = new FileStream($@"c:\{fileName}\xmlfile.xml", FileMode.Create))
+            {
+                myxmlSerializer.Serialize(file, car);
+            }
+        }
+        public static void SerializeACarArray(string fileName, Car[] cars)
+        {
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car[]));
+            using (Stream file = new FileStream($@"c:\{fileName}\xmlfile.xml", FileMode.Create))
+            {
+                myxmlSerializer.Serialize(file, cars);
+            }
+        }
+        public static Car DeserializeACar(string fileName)
+        {
+            Car car;
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car));
+            using (Stream file = new FileStream($@"c:\{fileName}\xmlfile.xml", FileMode.Open))
+            {
+                car = myxmlSerializer.Deserialize(file) as Car;
+            }
+            return car;
+        }
+        public static Car[] DeserializeACarArray(string fileName)
+        {
+
+            Car[] cars;
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car[]));
+            using (Stream file = new FileStream($@"c:\{fileName}\xmlfile.xml", FileMode.Open))
+            {
+                cars = myxmlSerializer.Deserialize(file) as Car[];
+            }
+            return cars;
+        }
+        public bool CarCompare(string fileName)
+        {
+            Car car;
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car));
+            using (Stream file = new FileStream($@"c:\{fileName}\xmlfile.xml", FileMode.Open))
+            {
+                car = myxmlSerializer.Deserialize(file) as Car;
+            }
+            if (this.Model == car.Model&& this.Brand == car.Brand&& this.Year == car.Year&& this.Color == car.Color)
+            {
+                return true;
+                       
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Car model:{this.Model,5} Brand:{this.Brand,2} Year:{this.Year,5} Color:{this.Color,5} codan:{this.codan} number of seats:{this.numberOfSeats}";
+        }
+    }
+}
---
+++ 24.2.19 homework/24.2.19 homework/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _24._2._19_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars = {new Car("mazda", 3, 2011, "blue", 5332, 5),
+            new Car("nissan",6,2000,"red",2332,6),new Car("ford",7,2008,"gray",2556,7)};
+            //Car mazda = new Car("mazda", 3, 2011, "blue", 5332, 5);
+            //Console.WriteLine(mazda);
+            XmlSerializer myxmlSerializer = new XmlSerializer(typeof (Car[]));
+            using (Stream file = new FileStream(@"c:\temp\xmlfile.xml", FileMode.Create))
+            {
+              myxmlSerializer.Serialize(file, cars);
+            }
+            PrintArray(cars);
+            Car [] cars2;
+            //XmlSerializer myxmlSerializer = new XmlSerializer(typeof(Car[]));
+            using (Stream file = new FileStream(@"c:\temp\xmlfile.xml", FileMode.Open))
+            {
+                cars2 = myxmlSerializer.Deserialize(file) as Car[];
+            }
+            PrintArray(cars2);
+        }
+        static void PrintArray(Car [] c)
+        {
+            foreach  (Car car in c)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
